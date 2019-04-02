1db5764c165ea808eac3b2dafbd5afbfa84a3294
git-trading <kba.asm.12@gmail.com>
2019-04-02 12:43:46 +0800

    added gulp4notes.js

---
+++ gulp4notes.js
@@ -0,0 +1,64 @@
+const { series, parallel } = require('gulp');
+
+////// with process.env.NODE_ENV
+
+function minify(cb) {
+  // body omitted
+  cb();
+}
+
+function transpile(cb) {
+  // body omitted
+  cb();
+}
+
+function livereload(cb) {
+  // body omitted
+  cb();
+}
+
+if (process.env.NODE_ENV === 'production') {
+  exports.build = series(transpile, minify);
+} else {
+  exports.build = series(transpile, livereload);
+}
+
+/*
+// This is INCORRECT
+const clean = function(cb) {
+  // body omitted
+  cb();
+};
+
+const css = series(clean, function(cb) {
+  // body omitted
+  cb();
+});
+
+const javascript = series(clean, function(cb) {
+  // body omitted
+  cb();
+});
+
+exports.build = parallel(css, javascript);
+
+*/
+
+// MIGRATE to this:
+
+function clean(cb) {
+  // body omitted
+  cb();
+};
+
+function css(cb) {
+  // body omitted
+  cb();
+}
+
+function javascript(cb) {
+  // body omitted
+  cb();
+}
+
+exports.build = series(clean, parallel(css, javascript))
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
+/* the old way
 var gulp = require('gulp'),
 watch = require('gulp-watch');
 
-const { series } = require('gulp');
-
-/* the old way
 gulp.task('default', function(done) {
   // kailangan 'yung done na argument sa v4.0 ng gulp
   console.log('Hooray - you created a Gulp task.');
@@ -16,16 +14,26 @@ gulp.task('html', function(done) {
 });
 
 gulp.task('watch', function() {
-
+  
   watch('./app/index.html', function(events, done) {
     gulp.start('html', done)
   });
-
+  
 });
 */
 
+const { watch, series, parallel } = require('gulp');
+
 function defaultTask(cb) {
+  console.log('you created a gulp task');
+  cb();
+};
+
+function html(cb) {
+  console.log('doing something with the html')
   cb();
 }
 
-exports.default = defaultTask
+watch('./app/index.html', html)
+
+exports.build = series(defaultTask, html)
