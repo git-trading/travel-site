ae85eaf38eb05b1053c691223d71c9b42c16e298
git-trading <kba.asm.12@gmail.com>
2019-04-03 16:09:45 +0800

    added a lot of css and a magical browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -22,18 +22,47 @@ gulp.task('watch', function() {
 });
 */
 
-const { watch, series, parallel } = require('gulp');
+const { watch, series, src, dest } = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
-function defaultTask(cb) {
-  console.log('you created a gulp task');
-  cb();
-};
+// function defaultTask(cb) {
+//   console.log('you created a gulp task');
+//   cb();                           
+// };
 
 function html(cb) {
-  console.log('doing something with the html')
+  console.log('reloading...');
+  browserSync.reload();
   cb();
 }
 
-watch('./app/index.html', html)
+function styles() {
+  return src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer])) // order of plugins is important
+    .pipe(dest('./app/temp/styles/'))
+    .pipe(browserSync.stream())
+}
 
-exports.build = series(defaultTask, html)
+// watch('./app/index.html', html); // matik nang triggered ito kapag nag-gulp bsata nasa labas
+// watch('./app/assets/styles/**/*.css', styles);
+// exports.default = series(defaultTask, html);
+
+function watchAll() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: './app' // points to where index.html lives
+    }
+  });
+  watch('./app/index.html', html); // matik nang triggered ito kapag nag-gulp
+  watch('./app/assets/styles/**/*.css', styles);
+}
+
+
+exports.default = series(html, watchAll);
+exports.styles = styles;
