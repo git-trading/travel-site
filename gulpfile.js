f29a13f46c2ced629e4de305df4c1ee44447555d
git-trading <kba.asm.12@gmail.com>
2019-04-04 15:17:30 +0800

    Completed large hero block and intro content block.

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
 browserSync = require('browser-sync').create();
 
 function styles() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer])) // order of plugins is important
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) // order of plugins is important
     .pipe(gulp.dest('./app/temp/styles/'))
 }
 
