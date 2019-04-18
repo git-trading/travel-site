da77befd6f407c8fac8fce3e34cbfee5988b406e
git-trading <kba.asm.12@gmail.com>
2019-04-18 14:41:28 +0800

    Completed styling site header.

--- gulpfile.js/styles.js
+++ gulpfile.js/styles.js
@@ -1,17 +1,17 @@
 const gulp = require('gulp'),
-browserSync = require('browser-sync').create(),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 
 function styles() {
   return gulp.src('./app/assets/styles/styles.css')
     // order of plugins in postcss is important
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) 
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer])) 
     .pipe(gulp.dest('./app/temp/styles/'))
 }
 
