c10825d4751a3f0752a9f801440cabecd2ed7dae
git-trading <kba.asm.12@gmail.com>
2019-04-18 12:10:21 +0800

    Completed automatic sprite generation.

--- gulpfile.js/sprites.js
+++ gulpfile.js/sprites.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
  
 var config = {
   mode: {
@@ -15,6 +16,13 @@ var config = {
   }
 };
 
+// deletes anything on app/temp/sprite and
+// app/assets/images/sprites
+// before actually creating new svg and css resources
+function beginClean() {
+  return del(['./app/temp/sprite', './app/assets/images/sprites']);
+};
+
 // creating a consolidated svg file from the pile of svg
 // files (for icons) in the app/assets/images/icons subdirectory
 function createSprite() {
@@ -22,14 +30,14 @@ function createSprite() {
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
 };
-// exports.createSprite = createSprite;
+// exports.createSprite = createSprite; // no need to export this
 
 // copying the created sprite.css
 // from createSprite task to app/assets/images/sprites
 function copySpriteGraphic() {
   return gulp.src('./app/temp/sprite/css/**/*.svg')
   .pipe(gulp.dest('./app/assets/images/sprites/'));
-}
+};
 
 // the copied sprite.css will be copied yet again to
 // app/assets/styles/modules and named _sprite.css
@@ -37,7 +45,21 @@ function copySpriteCSS() {
   return gulp.src('./app/temp/sprite/css/*.css')
   .pipe(rename('_sprite.css'))
   .pipe(gulp.dest('./app/assets/styles/modules/'));
-}
-// exports.copySpriteCSS = copySpriteCSS;
+};
+// exports.copySpriteCSS = copySpriteCSS; // no need to export this
 
-exports.icons = gulp.series(createSprite, copySpriteGraphic, copySpriteCSS);
+// the created directory app/temp/sprite and
+// all children files and directories are deleted kasi nalipat na sa 
+// assets/images/sprites and assets/styles/modules accordingly
+// during the execution of copySpriteGraphic and copySpriteCSS tasks
+function endClean() {
+  return del(['./app/temp/sprite'])
+};
+
+exports.icons = gulp.series(
+  beginClean,
+  createSprite, 
+  copySpriteGraphic, 
+  copySpriteCSS,
+  endClean
+);
